6d3cd5d6e5a06c15f14e51f51184f43e8ea30aa0
mark yasser <markyasser2011@gmail.com>
2022-03-13 18:31:06 +0200

    Updated results page

--- Website/app.js
+++ Website/app.js
@@ -22,7 +22,7 @@ var app = express();
 app.set('view engine','ejs');
 
 //static files
-app.use(express.static('./assets'));
+app.use(express.static('../Website'));
 
 //fire controller, send to it an instance of app
 searchController(app);
