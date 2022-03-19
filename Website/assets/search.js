caa6468a66a3b2059ab1bf9015c9b1d51ff5c5a1
mark yasser <markyasser2011@gmail.com>
2022-03-19 15:54:24 +0200

    responsive search bar (not complete)

--- Website/assets/search.js
+++ Website/assets/search.js
@@ -1,5 +1,54 @@
 console.log("connected");
 $(document).ready(function(){
+  //--------------Search Bar Responsive Events--------------//
+  //to make onclick event
+  var isPressed = false;
+  $(document).mouseup(function(e){
+    var container = $("form");
+    // If the target of the click isn't the container
+    if(!container.is(e.target) && container.has(e.target).length === 0){
+      isPressed=false;
+      $(this).css("background-color", "#121212");
+      $('.searchBox .search').css("background-color", "#121212");
+      $('.searchBox').css("background-color", "#121212");
+      $('form').css("border", "1px solid #808080");
+      $('ul').hide();
+    }
+});
+  $('form').click(function(){
+    isPressed=true;
+    $(this).css("background-color", "#303030");
+    $('.searchBox .search').css("background-color", "#303030");
+    $('.searchBox').css("background-color", "#303030");
+    $('form').css("border", "1px solid #303030");
+    $('ul').show();
+  });
+  //to make hover event
+  $('form').mouseover(function(){
+    $(this).css("background-color", "#303030");
+    $('.searchBox .search').css("background-color", "#303030");
+    $('.searchBox').css("background-color", "#303030");
+    $('form').css("border", "1px solid #303030");
+ 
+  });
+  $('form').mouseout(function(){
+    if(!isPressed)
+    {
+      $(this).css("background-color", "#121212");
+      $('.searchBox .search').css("background-color", "#121212");
+      $('.searchBox').css("background-color", "#121212");
+      $('form').css("border", "1px solid #808080");
+    }
+  });
+  //--------------------------------------------------------//
+  //----------------ON CLICK ON THE RESULT------------------//
+  $('ul li').click(function(){
+    window.alert(item);
+    item =  $('ul li').val();
+    $('#searchWord').val(item);
+    
+  });
+  //--------------------------------------------------------//
     //On key up event
     $('#searchWord').on('keyup', function(){
         //var that holds the text input
@@ -23,7 +72,10 @@ $(document).ready(function(){
             $('ul').empty();
             for (let i = 0; i < data.length; i++) {
                 //append new results to list
-                $('ul').append(`<li>${data[i]._TITLE}</li>`);
+                if(i+1 != data.length)
+                  $('ul').append(`<li>${data[i]._TITLE}</li>`);
+                else
+                  $('ul').append(`<li style="border-bottom-right-radius:20px;border-bottom-left-radius:20px">${data[i]._TITLE}</li>`);
             }
           }
         });
