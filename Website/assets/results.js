cceacfbab54d20386e6c5937e6d735e35ad2c040
doaa281 <65799105+doaa281@users.noreply.github.com>
2022-03-21 23:55:01 +0200

    lastresults

---
+++ Website/assets/results.js
@@ -0,0 +1,50 @@
+var pages = 0;
+var indexy = 0;
+// if (blogs.length > 5) {
+//     pages = blogs.length / 5;
+
+// }
+// else pages = 1;
+
+// for(let j=1;j<pages;j++)
+// {
+//     $(".btns").append(`<button>${j}</button>`);
+
+// }
+
+
+
+    
+$('.bb').on("click",()=>{
+    var btn = $("bb");
+  var starting=  (btn.val()-1)*5;
+var arr=[];
+var items = {starting,arr};
+  $.ajax({
+      type: "POST",
+      url: "/results",
+      data: items,
+      
+      success:function(data)
+      {
+          console.log("jnjnjbhbhb");
+          $(".cont").empty();
+            for(let i=0;i<5;i++)
+            {
+                $(".in-tit").html(items.minib[i].title);
+                $(".in-sum").html(items.minib[i].snippet);
+            }
+            
+            location.reload();
+
+      },
+     error: function(x,y){
+console.log(x);
+console.log(y);
+
+     }
+  });
+});
+
+var begin=0;
+var end;
