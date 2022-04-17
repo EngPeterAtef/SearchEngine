bd9947be6843ea21724d3a11c31cdc6bbd675347
Bemoi Erian <bemoi.erian@gmail.com>
2022-04-18 01:13:03 +0200

    Started crawler, read seeds from json files and write html to data file.

--- Engine/src/main/java/Main.java
+++ Engine/src/main/java/Main.java
@@ -1,7 +1,7 @@
 import Database.Controller;
 public class Main {
     public static void main(String[] args) {
-        Controller MongoDB = new Controller();
-        MongoDB.ControllerTest();
+//        Controller MongoDB = new Controller();
+//        MongoDB.ControllerTest();
     }
 }
