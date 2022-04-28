c4a2ee16e59887a4e4c9cae7ed3d3ae3732eaad9
doaa281 <65799105+doaa281@users.noreply.github.com>
2022-04-28 18:31:24 +0200

    indexe

--- Engine/src/main/java/Crawler/Crawler.java
+++ Engine/src/main/java/Crawler/Crawler.java
@@ -1,188 +1,188 @@
-package Crawler;
+ package Crawler;
 
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+ import com.google.gson.*;
+ import com.google.gson.reflect.TypeToken;
+ import org.jsoup.Connection;
+ import org.jsoup.Jsoup;
+ import org.jsoup.nodes.Document;
+ import org.jsoup.nodes.Element;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
+ import java.io.*;
+ import java.nio.file.Files;
+ import java.nio.file.Paths;
+ import java.util.ArrayList;
+ import java.util.List;
 
-public class Crawler implements Runnable{
-    //Maximum depth of crawler recursive calls
-    private static final int MAX_DEPTH = 2;
-    //Maximum number of total visited pages, 5000 in this project
-    private static final int MAX_PAGES = 10;
-    //number of threads used
-    private static int NUM_THREADS = 2;
-    //first link to start crawling from
-    private String first_link;
-    //list of visited links to not visit a link again
-    private ArrayList<String> visitedLinks = new ArrayList<String>();
-    //ID of current thread
-    private int ID;
-    //To count number of visited pages
-    private static int Counter;
-    //list of urls existing in queue
-    private static List<URLQueue> URLs = null;
-    private static List<Data> CollectedData = null;
+ public class Crawler implements Runnable{
+     //Maximum depth of crawler recursive calls
+     private static final int MAX_DEPTH = 2;
+     //Maximum number of total visited pages, 5000 in this project
+     private static final int MAX_PAGES = 10;
+     //number of threads used
+     private static int NUM_THREADS = 2;
+     //first link to start crawling from
+     private String first_link;
+     //list of visited links to not visit a link again
+     private ArrayList<String> visitedLinks = new ArrayList<String>();
+     //ID of current thread
+     private int ID;
+     //To count number of visited pages
+     private static int Counter;
+     //list of urls existing in queue
+     private static List<URLQueue> URLs = null;
+     private static List<Data> CollectedData = null;
 
-    //class to specify the structure of QUEUE JSON objects
-    private class URLQueue{
-        public String url;
-        public boolean visited;
-        public int threadID;
-        URLQueue(String url, boolean visited, int threadID){
-            this.url = url;
-            this.visited = visited;
-            this.threadID = threadID;
-        }
-    }
-    //class to specify the structure of HTML (Collected data) JSON objects
-    private class Data{
-        public String url;
-        public String html;
-        Data(String url, String html){
-            this.url = url;
-            this.html = html;
-        }
-    }
+     //class to specify the structure of QUEUE JSON objects
+     private class URLQueue{
+         public String url;
+         public boolean visited;
+         public int threadID;
+         URLQueue(String url, boolean visited, int threadID){
+             this.url = url;
+             this.visited = visited;
+             this.threadID = threadID;
+         }
+     }
+     //class to specify the structure of HTML (Collected data) JSON objects
+     private class Data{
+         public String url;
+         public String html;
+         Data(String url, String html){
+             this.url = url;
+             this.html = html;
+         }
+     }
 
-    public Crawler(String link){
-        first_link = link;
-    }
-    public void run()
-    {
-        this.ID = Integer.parseInt(Thread.currentThread().getName());
-        crawl(1, first_link);
-    }
-    private void crawl(int level , String url){
-        if(level <= MAX_DEPTH && Counter <= MAX_PAGES){
-            Document doc = request(url);
-            if(doc != null)
-            {
-                //-----INSERT DATA TO FILE----
-                Data data = new Data(url, doc.outerHtml());
-                WriteToDataFile(data);
-                //-------GET OTHER LINKS------
-                for(Element link :  doc.select("a[href]")){
-                    String next_link = link.absUrl("href");
-                    if(visitedLinks.contains(next_link) == false){
-                        crawl(level++,next_link);
-                    }
-                }
-            }
-        }
-    }
-    private Document request(String url)
-    {
-        try {
-            Connection con = Jsoup.connect(url);
-            Document doc = con.get();
-            if(con.response().statusCode() == 200)
-            {
-//                System.out.println("Bot ID : "+ID+" Recieved Webpage at : "+url);
-                URLQueue link = new URLQueue(url, false, ID);
-                WriteToQueueFile(link);
-                String title = doc.title();
-//                System.out.println(title);
-//                WriteToFile(title);
-                visitedLinks.add(url);
-//                Counter++;
-                return doc;
-            }
-            return null;
-        } catch (IOException e) {
-            return null;
+     public Crawler(String link){
+         first_link = link;
+     }
+     public void run()
+     {
+         this.ID = Integer.parseInt(Thread.currentThread().getName());
+         crawl(1, first_link);
+     }
+     private void crawl(int level , String url){
+         if(level <= MAX_DEPTH && Counter <= MAX_PAGES){
+             Document doc = request(url);
+             if(doc != null)
+             {
+                 //-----INSERT DATA TO FILE----
+                 Data data = new Data(url, doc.outerHtml());
+                 WriteToDataFile(data);
+                 //-------GET OTHER LINKS------
+                 for(Element link :  doc.select("a[href]")){
+                     String next_link = link.absUrl("href");
+                     if(visitedLinks.contains(next_link) == false){
+                         crawl(level++,next_link);
+                     }
+                 }
+             }
+         }
+     }
+     private Document request(String url)
+     {
+         try {
+             Connection con = Jsoup.connect(url);
+             Document doc = con.get();
+             if(con.response().statusCode() == 200)
+             {
+ //                System.out.println("Bot ID : "+ID+" Recieved Webpage at : "+url);
+                 URLQueue link = new URLQueue(url, false, ID);
+                 WriteToQueueFile(link);
+                 String title = doc.title();
+ //                System.out.println(title);
+ //                WriteToFile(title);
+                 visitedLinks.add(url);
+ //                Counter++;
+                 return doc;
+             }
+             return null;
+         } catch (IOException e) {
+             return null;
 
-        }
-    }
+         }
+     }
 
-    public static void main(String[] arg)
-    {
-        Crawler crawlerObj = new Crawler("");
-        try {
-            // create Gson instance
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+     public static void main(String[] arg)
+     {
+         Crawler crawlerObj = new Crawler("");
+         try {
+             // create Gson instance
+             Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-            // create a reader
-            Reader reader = Files.newBufferedReader(Paths.get("queue.json"));
+             // create a reader
+             Reader reader = Files.newBufferedReader(Paths.get("queue.json"));
 
-            // convert JSON array to list of users
-            URLs = gson.fromJson(reader, new TypeToken<List<URLQueue>>(){}.getType());
-            reader.close();
+             // convert JSON array to list of users
+             URLs = gson.fromJson(reader, new TypeToken<List<URLQueue>>(){}.getType());
+             reader.close();
 
-            //--IF THE FILE IS EMPTY, INSERT SEEDS--
-            if (URLs == null) {
-                URLQueue link1= crawlerObj.new URLQueue("https://www.wikipedia.org/", false, 0);
-                URLQueue link2= crawlerObj.new URLQueue("https://www.nytimes.com/", false, 0);
-                WriteToQueueFile(link1);
-                WriteToQueueFile(link2);
-            }
-            //--FILE IS NOT EMPTY, THEN CONTINUE FROM LAST STATE
-            else {
+             //--IF THE FILE IS EMPTY, INSERT SEEDS--
+             if (URLs == null) {
+                 URLQueue link1= crawlerObj.new URLQueue("https://www.wikipedia.org/", false, 0);
+                 URLQueue link2= crawlerObj.new URLQueue("https://www.nytimes.com/", false, 0);
+                 WriteToQueueFile(link1);
+                 WriteToQueueFile(link2);
+             }
+             //--FILE IS NOT EMPTY, THEN CONTINUE FROM LAST STATE
+             else {
 
-                /********
-                 * STILL NOT COMPLETE
-                 * ******/
+                 /********
+                  * STILL NOT COMPLETE
+                  * ******/
 
-//                URLQueue link3= crawlerObj.new URLQueue("https://www.google.com/", false, 0);
-//                WriteToFile(link3);
-                Counter = URLs.size();
-                System.out.println(URLs.get(0).url);
-            }
+ //                URLQueue link3= crawlerObj.new URLQueue("https://www.google.com/", false, 0);
+ //                WriteToFile(link3);
+                 Counter = URLs.size();
+                 System.out.println(URLs.get(0).url);
+             }
 
-            // close reader
-            reader.close();
+             // close reader
+             reader.close();
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+         } catch (Exception ex) {
+             ex.printStackTrace();
+         }
 
-        Thread[] threads = new Thread[NUM_THREADS];
-        String[] startLinks = new String[NUM_THREADS];
+         Thread[] threads = new Thread[NUM_THREADS];
+         String[] startLinks = new String[NUM_THREADS];
 
-        for (int i = 0; i < NUM_THREADS; i++) {
-            startLinks[i] = URLs.get(i).url;
-//            System.out.println(startLinks[i]);
-            threads[i] = new Thread(new Crawler(startLinks[i]));
-            threads[i].setName(String.valueOf(i + 1));
-            threads[i].start();
-        }
-    }
-    private static void WriteToQueueFile(URLQueue obj){
-        if (URLs == null){
-            URLs = new ArrayList<URLQueue>();
-        }
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Writer writer = null;
-        try {
-            writer = Files.newBufferedWriter(Paths.get("queue.json"));
-            URLs.add(obj);
-            gson.toJson(URLs, writer);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    private static void WriteToDataFile(Data obj){
-        if (CollectedData == null){
-            CollectedData = new ArrayList<Data>();
-        }
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Writer writer = null;
-        try {
-            writer = Files.newBufferedWriter(Paths.get("data.json"));
-            CollectedData.add(obj);
-            gson.toJson(CollectedData, writer);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+         for (int i = 0; i < NUM_THREADS; i++) {
+             startLinks[i] = URLs.get(i).url;
+ //            System.out.println(startLinks[i]);
+             threads[i] = new Thread(new Crawler(startLinks[i]));
+             threads[i].setName(String.valueOf(i + 1));
+             threads[i].start();
+         }
+     }
+     private static void WriteToQueueFile(URLQueue obj){
+         if (URLs == null){
+             URLs = new ArrayList<URLQueue>();
+         }
+         Gson gson = new GsonBuilder().setPrettyPrinting().create();
+         Writer writer = null;
+         try {
+             writer = Files.newBufferedWriter(Paths.get("queue.json"));
+             URLs.add(obj);
+             gson.toJson(URLs, writer);
+             writer.close();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+     private static void WriteToDataFile(Data obj){
+         if (CollectedData == null){
+             CollectedData = new ArrayList<Data>();
+         }
+         Gson gson = new GsonBuilder().setPrettyPrinting().create();
+         Writer writer = null;
+         try {
+             writer = Files.newBufferedWriter(Paths.get("data.json"));
+             CollectedData.add(obj);
+             gson.toJson(CollectedData, writer);
+             writer.close();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+ }
--- Engine/src/main/java/Indexer/Indexer.java
+++ Engine/src/main/java/Indexer/Indexer.java
@@ -1,4 +1,304 @@
 package Indexer;
+import opennlp.tools.stemmer.PorterStemmer;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+// import org.jsoup.Connection;
+// import org.jsoup.Jsoup;
+// import org.jsoup.nodes.Document;
+// import org.jsoup.nodes.Element;
+//import java.lang.*;
+
+
+
+//import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.*;
+import java.util.Map.Entry;
 
 public class Indexer {
+    private static List<Data> CollectedData = null;// areay of websirtes
+    private static HashMap<String, HashMap<String, pair>> indexermap = new HashMap<String, HashMap<String, pair>>();
+    // word //url //occurrences
+    private static List<String> stopwords = null;
+
+    // class to specify the structure of HTML (Collected data) JSON objects
+    public class Data {
+        public String url;
+        public String html;
+
+        Data(String url, String html) {
+            this.url = url;
+            this.html = html;
+        }
+    }
+
+    public static class pair {
+        public Integer count;
+        // public BitSet location ;
+        public BitSet location;
+
+        pair() {
+
+          location=new BitSet(15);
+            count = 0;
+           // location = new char[15];
+          //  location = 0;
+            // this.count = count;
+            // this.location = location;
+        }
+    }
+
+//    static void setlocations(String mytag, Integer locations) {
+//        switch (mytag) {
+//            case "title":
+//                locations =1;
+//                System.out.println("yes");break;
+//            case "h1":
+//                locations = 2;
+//                System.out.println("yes");break;
+//            case "h2":
+//                locations= 3;
+//                System.out.println("yes");break;
+//            case "h3":
+//                locations= 4;
+//                System.out.println("yes");break;
+//                case "h4":
+//                locations=5;
+//                System.out.println("yes");break;
+//            case "h5":
+//                locations=6;
+//                System.out.println("yes");break;
+//            case "h6":
+//                locations=7;
+//                System.out.println("yes");break;
+//            case "nav":
+//                locations=8;
+//                System.out.println("yes");break;
+//            case "header":
+//                locations=9;
+//                System.out.println("yes");break;
+//            case "li":
+//                locations=10;
+//                System.out.println("yes");break;
+//            case "main":
+//                locations=11;
+//                System.out.println("yes");break;
+//            case "section":
+//                locations=12;
+//                System.out.println("yes"+locations.toString());break;
+//            case "p":
+//                locations=13;
+//                System.out.println("yes");break;
+//            case "side":
+//                locations=14;
+//                System.out.println("yes");break;
+//            case "footer":
+//                locations=15;
+//                System.out.println("yes");break;
+//                case "<div>":
+//                locations=16;
+//                System.out.println("yes");break;
+//            default:
+//                System.out.println("no");break;
+//        }
+//        return;
+//    }
+
+    static void setlocations(String mytag, BitSet locations) {
+        switch (mytag) {
+            case "title":
+                locations.set(14);
+                break;
+            case "h1":
+
+                locations.set(13);
+                break;
+            case "h2":
+                locations.set(12);
+                break;
+            case "h3":
+                locations.set(11);
+                break;
+            case "h4":
+                locations.set(10);
+                break;
+            case "h5":
+                locations.set(9);
+                break;
+            case "h6":
+                locations.set(8);
+                break;
+            case "nav":
+                locations.set(7);
+                break;
+            case "header":
+                locations.set(6);
+                break;
+            case "li":
+                locations.set(5);
+                break;
+            case "main":
+                locations.set(4);
+                break;
+            case "section":
+                locations.set(3);
+                break;
+            case "p":
+                locations.set(2);
+                break;
+            case "side":
+                locations.set(1);
+                break;
+            case "footer":
+                locations.set(0);
+                break;
+            default:
+                break;
+
+        }
+        return;
+
+    }
+
+    public static void main(String[] arg) {
+        try {
+            // create Gson instance
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            // create a reader
+            Reader reader = Files.newBufferedReader(Paths.get("E:/Koleya/APT/proj/SearchEngine/Engine/data.json"));
+            CollectedData = gson.fromJson(reader, new TypeToken<List<Data>>() {
+            }.getType());// array of json objects websites
+            reader.close();
+            // array of stop words
+            stopwords = Files.readAllLines(Paths.get("E:/Koleya/APT/proj/SearchEngine/Engine/stop_words_english.txt"));
+
+            // intilaise alll words strings
+            LinkedHashSet<String> allWords = new LinkedHashSet<String>();
+            LinkedHashSet<String> allWords2 = new LinkedHashSet<String>();
+
+            for (Data w : CollectedData) {
+                allWords.addAll(Stream.of(Jsoup.parse(w.html).text().toLowerCase().split(" "))
+                        .collect(Collectors.toCollection(LinkedHashSet<String>::new)));// array of all words in page
+
+            }
+            PorterStemmer p = new PorterStemmer();
+
+            FileWriter writerr = new FileWriter("E:/Koleya/APT/proj/SearchEngine/Engine/output.txt");
+
+
+
+
+            allWords.removeAll(stopwords);// remove all blabla
+//            for (String element : allWords)
+//            {
+//                // Print the elements of LinkedHashSet created above
+//                allWords.add( p.stem(element));
+//               // allWords.remove(element);
+//
+//        }
+
+            Iterator iter = allWords.iterator();
+            while (iter.hasNext())
+            {
+               allWords2.add(p.stem(iter.next().toString())) ;
+
+            }
+            for (String elemett:allWords2)
+            {
+                writerr.write(elemett+System.lineSeparator());
+            }
+            ////////////////////////////////////////////
+
+            // h1 h2 h3 h4
+            // 0 0 0 0
+            // 1 0 0 0
+
+            // hero ->wikipidea->hero title
+            // hero footer
+            // hero body
+            // yahoo
+            // facebook
+            // vilian
+        //     FileWriter writerr = new FileWriter("E:/Koleya/APT/proj/SearchEngine/Engine/output.txt");
+        //    // Document doc = Jsoup.parse(CollectedData.get(0).html);
+        //     writerr.write(CollectedData.get(0).html.replaceAll("\\<.*?\\>", ""));
+
+
+/*
+             for (String aword : allWords)// ma3aya kelma
+             {
+               // String aword="ukraine";
+                HashMap <String,pair>tempo = new HashMap<String, pair>();
+
+                // indexermap.put(aword,tempo);//add my word
+                for (Data w : CollectedData)// le kol doc
+                {
+                    pair temppair = new pair();
+                    Document doc = Jsoup.parse(w.html);
+
+                   // int counter = 0;
+
+                    for (Element e : doc.select("*:containsOwn(" + aword + ")")) {// kol el elements ely gowahom el
+                                                                                  // kelma dyh
+                        // indexermap.get(aword).get(w.url).count++;//increment occurence
+                        temppair.count++;
+                        for (Element h : e.parents())// each tag
+                        {
+                           // System.out.println(h.tagName());
+                            setlocations(h.tagName(), temppair.location);// set all locations
+
+
+                        }
+
+                    }
+
+                    tempo.put(w.url, temppair);
+                }
+                indexermap.put(aword, tempo);
+               // System.out.println("hello there");
+
+             }
+            // HashMap te=new HashMap<String,char[]>();
+            // te.put("mimi",'y');
+            // indexermap.put("hello",te);
+
+            FileWriter writer = new FileWriter("E:/Koleya/APT/proj/SearchEngine/Engine/tm.txt");
+           
+writer.write("WORD       URL          count         locations\n");
+            Iterator<Entry<String, HashMap<String, pair>>> it = indexermap.entrySet().iterator();
+
+            while (it.hasNext()) {
+                // System.out.println("nono");
+                Entry<String, HashMap<String, pair>> entry = it.next();
+                writer.write(entry.getKey()+" ------> ");
+                Iterator<Entry<String, pair>> itt = entry.getValue().entrySet().iterator();
+                while (itt.hasNext()) {
+                    Entry<String, pair> en2 = itt.next();
+                   String kk= IntStream.range(0, en2.getValue().location.length())
+                            .mapToObj(b -> String.valueOf(en2.getValue().location.get(b) ? 1 : 0))
+                            .collect(Collectors.joining());
+                   writer.write(en2.getKey()+" ------> ");
+                    writer.write(en2.getValue().count.toString()+" ---------> "+kk+System.lineSeparator());
+
+
+                }
+            }
+            writer.close();
+*/
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
 }
