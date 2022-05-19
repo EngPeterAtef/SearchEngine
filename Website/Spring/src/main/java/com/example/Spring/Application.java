cebf9bb29ee10e64d3fd212f275c04956046f483
mark yasser <markyasser2011@gmail.com>
2022-05-19 16:10:12 +0200

    Phrase search results working but still can't display them in results page

--- Website/Spring/src/main/java/com/example/Spring/Application.java
+++ Website/Spring/src/main/java/com/example/Spring/Application.java
@@ -98,20 +98,35 @@ public class Application {
 		}
 	}
 	Controller dbController = new Controller();
-	String ArrayOfquery;
+	String[] ArrayOfquery;
 	int c;
+	String query;
+//	String queryString;
+	PorterStemmer p = new PorterStemmer();
+	ArrayList<Document> matchingArray = new ArrayList<Document>();
+	ArrayList<Document> dataArray = new ArrayList<Document>();
 	@RestController
 	public class SpringController {
-		//------------SEARCHING-----------
+		//----------SAVE SUGGESTION IN DATABASE-----------
 		@RequestMapping(value="/search", method = RequestMethod.GET)
-		public String search(@RequestParam(value = "query") String query) {
-//			ArrayOfquery = query.split(" ");
-//			c =0;
-//			FindAndRank();
-
-			String ResultsPage = "<!doctype html> <html><body align=\"center\">"+ query +"</body></html>";
-			return ResultsPage;
+		void SaveSuggestion(@RequestParam(value = "query") String query) {
+			dbController.InsertOrUpdateSuggestion(query);
 		}
+		//------------SEARCHING-----------
+		@RequestMapping(value="/results/{query}", method = RequestMethod.GET)
+		String search(@PathVariable(value = "query") String query) {
+			ArrayOfquery = query.split(" ");
+			c = 0;
+			dataArray.clear();
+			matchingArray.clear();
+			FindAndRank(query);
+			String ResultsPage = "<!doctype html> <html><body align=\"center\">"+ dataArray +"</body></html>";
+			return ResultsPage;
+//			ModelAndView modelAndView = new ModelAndView();
+//			modelAndView.setViewName("result.html");
+//			return modelAndView;
+		}
+
 		//------------SUGGESTIONS------------
 		@RequestMapping(value="/search", method = RequestMethod.POST)
 		public List<String> suggest(@RequestParam(value = "query") String query) {
@@ -125,6 +140,71 @@ public class Application {
 			return modelAndView;
 		}
 
+	}
+	public void FindAndRank(String queryString) {
+		ArrayList<Document> resultArray = new ArrayList<Document>();
+
+		System.out.println("Original query" + queryString);
+		for (int i = 0; i < ArrayOfquery.length; i++) {
+			//porter stemmer
+			query = p.stem(ArrayOfquery[i].replaceAll("\"", "")).toLowerCase();
+			System.out.println("Query after stemming " + query);
+			//database
+			resultArray.add(dbController.GetWordFromIndexer(query));
+		}
+
+		//resultArray[0][0] contains object of 1st word
+		//resultArray[1][0] contains object of 2nd word
+		//resultArray[2][0] contains object of 3rd word
+		if (queryString.charAt(0) == '"' && queryString.charAt(queryString.length() - 1) == '"') {
+			String queryString_2 = queryString.replaceAll("\"", "");
+			for (int j = 0; j < resultArray.get(0).getList("Websites", Document.class).size(); j++) // loop on each url in the first word
+			{
+				Document siteResult = dbController.GetSiteFromCollectedData(resultArray.get(0).getList("Websites", Document.class).get(j).getString("URL"));
+				if (siteResult != null) {
+					int indx = siteResult.getString("html").indexOf(queryString_2);
+					int phraseCount = 0;
+					if (indx != -1) {
+						int startIndex = indx;
+						phraseCount++;
+
+						while ((indx = siteResult.getString("html").indexOf(queryString_2, startIndex)) > -1) {
+							phraseCount++;
+							startIndex = indx + queryString_2.length();
+						}
+						matchingArray.add(new Document().append("url", resultArray.get(0).getList("Websites", Document.class).get(j).getString("URL")).append("phraseCount", phraseCount));
+					}
+				}
+			}
+			double IDF = Math.log(5000.0 / matchingArray.size());
+			double TF = 0;
+			double TF_IDF = 0;
+			//get all the websites that has this word
+			for (int index = 0; index < matchingArray.size(); index++) {
+				Document siteResult = dbController.GetSiteFromWebsiteData(matchingArray.get(index).getString("url"));
+				if (siteResult != null) {
+					String str = siteResult.getString("body");
+					for (int i = 0; i < ArrayOfquery.length; i++) {
+						for (int j = 0; j < resultArray.get(i).getList("Websites", Document.class).size(); j++) {
+							if (matchingArray.get(index).getString("url").equals(resultArray.get(i).getList("Websites", Document.class).get(j).getString("URL"))) {
+								TF = resultArray.get(i).getList("Websites", Document.class).get(j).getInteger("Count") / (double) str.length();
+								TF_IDF += TF * IDF;
+//								System.out.println(matchingArray.get(index).getString("url") + " " + TF_IDF);
+							}
+						}
+					}
+					// let str = siteResult[0].body;
+					int indexOFquery = str.indexOf(query);
+					//resultArray[i][0].Websites[index].title = siteResult[0].title;
+					//resultArray[i][0].Websites[index].snippet = str.substring(Math.max(0,indexOFquery - 200),Math.min(indexOFquery + query.length + 300,str.length));
+					dataArray.add(new Document().append("url", matchingArray.get(index).getString("url")).append("title", siteResult.getString("title")).append("snippet", str.substring(Math.max(0, indexOFquery - 200), Math.min(indexOFquery + query.length() + 300, str.length()))));
+					//TF = resultArray[i][0].Websites[index].locations / str.length;
+					//TF_IDF = TF * IDF;
+					dataArray.set(c, dataArray.get(c).append("rank", TF_IDF + siteResult.getInteger("popularity") + matchingArray.get(index).getInteger("phraseCount")));
+					c++;
+				}
+			}
+		}
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
