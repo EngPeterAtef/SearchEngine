79b2ab28d98d3eee371da7c7f58f23df023c6051
Bemoi Erian <bemoi.erian@gmail.com>
2022-05-20 00:42:26 +0200

    Results page working, now remains page numbering, and to make sure that the results contain all the urls in the database

---
+++ Website/Spring/src/main/java/com/example/Spring/DBController.java
@@ -0,0 +1,94 @@
+package com.example.Spring;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoException;
+import com.mongodb.client.*;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class DBController {
+    String uri;
+    MongoClient mongoClient;
+    MongoDatabase database;
+    MongoCollection<Document> collectedDataCollection;
+    MongoCollection<Document> websiteDataCollection;
+    MongoCollection<Document> indexedDocumentsCollection;
+    MongoCollection<Document> suggestionsCollection;
+
+    //------to reduce console log-----
+    //    static Logger root = (Logger) LoggerFactory
+    //            .getLogger(Logger.ROOT_LOGGER_NAME);
+    //
+    //    static {
+    //        root.setLevel(Level.INFO);
+    //    }
+    public DBController(){
+        uri = "mongodb://localhost:27017";
+        mongoClient = MongoClients.create(uri);
+        //connect to db
+        database = mongoClient.getDatabase("SearchEngine");
+        collectedDataCollection = database.getCollection("CollectedData");
+        websiteDataCollection = database.getCollection("WebsiteData");
+        indexedDocumentsCollection = database.getCollection("Indexed_documents");
+        suggestionsCollection = database.getCollection("sugg-test");
+    }
+    public Document GetWordFromIndexer(String word){
+        return indexedDocumentsCollection.find(eq("Word", word)).first();
+    }
+    public Document GetSiteFromCollectedData(String url){
+        return collectedDataCollection.find(eq("url", url)).first();
+    }
+    public Document GetSiteFromWebsiteData(String url){
+        return websiteDataCollection.find(eq("url", url)).first();
+    }
+    public List<String> GetSuggestions(String query){
+        String patternStr = "^" + query;
+        Pattern pattern = Pattern.compile(patternStr, Pattern.CASE_INSENSITIVE);
+        Bson filter = Filters.regex("title", pattern);
+        List<String> suggestions = null;
+        //get an iterator to result
+        try (MongoCursor<Document> cursor = suggestionsCollection.find(filter).iterator()){
+            if (cursor.hasNext()) {
+                suggestions = new ArrayList<>();
+                while (cursor.hasNext()) {
+                    Document doc = cursor.next();
+                    suggestions.add(doc.getString("title"));
+                }
+            }
+            //            for (URLQueue queue : urlQueue) {
+            //                System.out.println(queue.url);
+            //            }
+        } catch (MongoException me) {
+            System.err.println("Unable to find queue due to an error: " + me);
+        }
+        return suggestions;
+    }
+    public void InsertOrUpdateSuggestion(String suggestion){
+        List<String> suggestions = GetSuggestions(suggestion);
+        try {
+            if (suggestions == null) {
+                suggestionsCollection.insertOne(new Document()
+                        .append("_id", new ObjectId())
+                        .append("title", suggestion)
+                        .append("counter", 1));
+            }
+            else{
+                Document query = new Document().append("title",  suggestion);
+                BasicDBObject incValue = new BasicDBObject("counter", 1);
+                BasicDBObject intModifier = new BasicDBObject("$inc", incValue);
+                suggestionsCollection.updateOne(query, intModifier);
+
+            }
+        } catch (MongoException me) {
+            System.err.println("Unable to increment suggestion due to an error: " + me);
+        }
+    }
+}
---
+++ Website/Spring/src/main/java/com/example/Spring/SpringController.java
@@ -0,0 +1,202 @@
+package com.example.Spring;
+
+import opennlp.tools.stemmer.PorterStemmer;
+import org.bson.Document;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class SpringController {
+    String[] ArrayOfquery;
+    int c;
+    String query;
+    //	String queryString;
+    PorterStemmer p = new PorterStemmer();
+    ArrayList<Document> matchingArray = new ArrayList<Document>();
+    ArrayList<Document> dataArray = new ArrayList<Document>();
+    DBController dbController = new DBController();
+    //----------SAVE SUGGESTION IN DATABASE-----------
+    @RequestMapping(value="/search", method = RequestMethod.GET)
+    public String SaveSuggestion(Model model,@RequestParam(value = "query") String query) {
+        dbController.InsertOrUpdateSuggestion(query);
+        ArrayOfquery = query.split(" ");
+        c = 0;
+        dataArray.clear();
+        matchingArray.clear();
+        FindAndRank(query);
+//        String ResultsPage = "<!doctype html> <html><body align=\"center\">"+ dataArray +"</body></html>";
+        model.addAttribute("query", query);
+        model.addAttribute("results", dataArray);
+        return "results";
+    }
+    //------------SEARCHING-----------
+    @RequestMapping(value="/results/{query}", method = RequestMethod.GET)
+    public String search(Model model, @PathVariable(value = "query") String query) {
+        ArrayOfquery = query.split(" ");
+        c = 0;
+        dataArray.clear();
+        matchingArray.clear();
+        FindAndRank(query);
+//        String ResultsPage = "<!doctype html> <html><body align=\"center\">"+ dataArray +"</body></html>";
+        model.addAttribute("query", query);
+        model.addAttribute("results", dataArray);
+        return "results";
+//			ModelAndView modelAndView = new ModelAndView();
+//			modelAndView.setViewName("results.html");
+//            modelAndView.addObject("results", dataArray);
+//			return modelAndView;
+//			return "redirect:/results/" + query;
+    }
+
+    //------------SUGGESTIONS------------
+    @RequestMapping(value="/search", method = RequestMethod.POST)
+    @ResponseBody
+    public List<String> suggest(@RequestParam(value = "query") String query) {
+        return dbController.GetSuggestions(query);
+    }
+    //---------DISPLAY SEARCH PAGE-------
+    @GetMapping(value="/")
+    public String index() {
+        return "search";
+    }
+
+
+    public void FindAndRank(String queryString) {
+        ArrayList<Document> resultArray = new ArrayList<Document>();
+
+        System.out.println("Original query" + queryString);
+        for (int i = 0; i < ArrayOfquery.length; i++) {
+            //porter stemmer
+            query = p.stem(ArrayOfquery[i].replaceAll("\"", "")).toLowerCase();
+            System.out.println("Query after stemming " + query);
+            //database
+            Document wordDoc = dbController.GetWordFromIndexer(query);
+            if (wordDoc != null) {
+
+                System.out.println("found query " + query);
+                resultArray.add(wordDoc);
+            }
+        }
+
+        //resultArray[0][0] contains object of 1st word
+        //resultArray[1][0] contains object of 2nd word
+        //resultArray[2][0] contains object of 3rd word
+        if (queryString.charAt(0) == '"' && queryString.charAt(queryString.length() - 1) == '"') {
+            String queryString_2 = queryString.replaceAll("\"", "");
+            if (!resultArray.isEmpty()){
+                for (int j = 0; j < resultArray.get(0).getList("Websites", Document.class).size(); j++) // loop on each url in the first word
+                {
+                    Document siteResult = dbController.GetSiteFromCollectedData(resultArray.get(0).getList("Websites", Document.class).get(j).getString("URL"));
+                    if (siteResult != null) {
+                        int indx = siteResult.getString("html").indexOf(queryString_2);
+                        int phraseCount = 0;
+                        if (indx != -1) {
+                            int startIndex = indx;
+                            phraseCount++;
+
+                            while ((indx = siteResult.getString("html").indexOf(queryString_2, startIndex)) > -1) {
+                                phraseCount++;
+                                startIndex = indx + queryString_2.length();
+                            }
+                            matchingArray.add(new Document().append("url", resultArray.get(0).getList("Websites", Document.class).get(j).getString("URL")).append("phraseCount", phraseCount));
+                        }
+                    }
+                }
+                double IDF = Math.log(5000.0 / matchingArray.size());
+                double TF = 0;
+                double TF_IDF = 0;
+                //get all the websites that has this word
+                for (int index = 0; index < matchingArray.size(); index++) {
+                    Document siteResult = dbController.GetSiteFromWebsiteData(matchingArray.get(index).getString("url"));
+                    if (siteResult != null) {
+                        String str = siteResult.getString("body");
+                        for (int i = 0; i < ArrayOfquery.length; i++) {
+                            for (int j = 0; j < resultArray.get(i).getList("Websites", Document.class).size(); j++) {
+                                if (matchingArray.get(index).getString("url").equals(resultArray.get(i).getList("Websites", Document.class).get(j).getString("URL"))) {
+                                    TF = resultArray.get(i).getList("Websites", Document.class).get(j).getInteger("Count") / (double) str.length();
+                                    TF_IDF += TF * IDF;
+                                    //								System.out.println(matchingArray.get(index).getString("url") + " " + TF_IDF);
+                                }
+                            }
+                        }
+                        // let str = siteResult[0].body;
+                        int indexOFquery = str.indexOf(query);
+                        //resultArray[i][0].Websites[index].title = siteResult[0].title;
+                        //resultArray[i][0].Websites[index].snippet = str.substring(Math.max(0,indexOFquery - 200),Math.min(indexOFquery + query.length + 300,str.length));
+                        dataArray.add(new Document().append("url", matchingArray.get(index).getString("url")).append("title", siteResult.getString("title")).append("snippet", str.substring(Math.max(0, indexOFquery - 200), Math.min(indexOFquery + query.length() + 300, str.length()))));
+                        //TF = resultArray[i][0].Websites[index].locations / str.length;
+                        //TF_IDF = TF * IDF;
+                        dataArray.set(c, dataArray.get(c).append("rank", TF_IDF + siteResult.getInteger("popularity") + matchingArray.get(index).getInteger("phraseCount")));
+                        c++;
+                    }
+                }
+            }
+        }
+
+        else
+        {
+            for (int i = 0; i < ArrayOfquery.length; i++)
+            {
+                if(resultArray.size() <= i)
+                {
+                    return;
+                }
+                if(resultArray.get(i).size() > 0)
+                {
+                    double IDF = Math.log(5000.0/resultArray.get(i).getList("Websites", Document.class).size());
+                    double TF = 0;
+                    double TF_IDF = 0;
+                    int Popularity = 1;
+                    //get all the websites that has this word
+                    for (int index = 0; index < resultArray.get(i).getList("Websites", Document.class).size(); index++)
+                    {
+                        Document siteResult = dbController.GetSiteFromWebsiteData(resultArray.get(i).getList("Websites", Document.class).get(index).getString("URL"));
+                        System.out.println(siteResult);
+                        if(siteResult != null)
+                        {
+                            String str = siteResult.getString("body");
+                            int indexOFquery = str.indexOf(query);
+                            resultArray.get(i).getList("Websites", Document.class).set(index,resultArray.get(i).getList("Websites", Document.class).get(index).append("title",siteResult.getString("title")));
+                            resultArray.get(i).getList("Websites", Document.class).set(index,resultArray.get(i).getList("Websites", Document.class).get(index).append("snippet",str.substring(Math.max(0,indexOFquery - 200),Math.min(indexOFquery + query.length() + 300,str.length()))));
+                            boolean alreadyExist = false;
+                            Popularity =  siteResult.getInteger("popularity");
+                            TF = resultArray.get(i).getList("Websites", Document.class).get(index).getInteger("Count") / (double)str.length();
+                            TF_IDF = TF * IDF;
+                            for (int k = 0; k < dataArray.size(); k++) {
+                                if (dataArray.get(k).getString("url").equals(resultArray.get(i).getList("Websites", Document.class).get(index).getString("URL")) ) {
+
+                                    alreadyExist = true;
+                                    double oldcount = dataArray.get(k).getDouble("count");
+                                    dataArray.set(k,dataArray.get(k).append("count",oldcount+1));
+
+                                    double oldrank = dataArray.get(k).getDouble("rank");
+                                    dataArray.set(k,dataArray.get(k).append("rank",oldrank + TF_IDF + 100));
+                                    System.out.println(dataArray.get(k).getString("url") + " " + dataArray.get(k).getString("count"));
+                                }
+                            }
+                            if (!alreadyExist) {
+                                //dataArray.push({"url": resultArray[i][0].Websites[index].URL, "title": resultArray[i][0].Websites[index].title, "snippet": resultArray[i][0].Websites[index].snippet, "count": 1});
+
+                                Document web_site = resultArray.get(i).getList("Websites", Document.class).get(index);
+                                String added_url = web_site.getString("URL");
+                                String added_title = web_site.getString("title");
+                                String added_snippet = web_site.getString("snippet");
+                                String added_count = web_site.getString("count");
+                                dataArray.add(new Document().append("url",added_url).append("title",added_title).append("snippet",added_snippet).append("count",added_count).append("rank",Popularity + TF_IDF));
+                                //dataArray[index].rank =  TF_IDF + Popularity;
+                                //double oldrank = dataArray.get(index).getDouble("rank");
+//								dataArray.set(index,dataArray.get(index).append("rank",Popularity + TF_IDF));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
