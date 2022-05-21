d8e047af8629a7a60bae342457c094070efbcfeb
Bemoi Erian <bemoi.erian@gmail.com>
2022-05-21 20:47:41 +0200

    deleted unnecessary files

--- Engine/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-import Database.Controller;
-public class Main {
-    public static void main(String[] args) {
-//        Controller MongoDB = new Controller();
-//        MongoDB.ControllerTest();
-    }
-}
